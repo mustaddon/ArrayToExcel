8be5ee262a7d47e3fdb279431651149eef9a54f1
Leonid Salavatov <mustaddon@gmail.com>
2020-02-04 20:36:56 +0300

    readme

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
-# ArrayToExcel
+# ArrayToExcel [![NuGet version](https://badge.fury.io/nu/ArrayToExcel.svg)](http://badge.fury.io/nu/ArrayToExcel)
 Create Excel from Array
+
+### Example #1
+
+```C#
+var items = Enumerable.Range(1, 10).Select(x => new
+{
+    Prop1 = $"Text #{x}",
+    Prop2 = x * 1000,
+    Prop3 = DateTime.Now.AddDays(-x),
+});
+
+var excel = items.ToExcel();
+```
+
+![](/Examples/example1.png)
+[example1.xlsx](Examples/example1.xlsx?raw=true)
+
+### Example #2
+
+```C#
+var excel = items.ToExcel(x => x
+    .AddColumn("MyColumnName#1", x => x.Prop1)
+    .AddColumn("MyColumnName#2", x => $"test:{x.Prop2}")
+    .AddColumn("MyColumnName#3", x => x.Prop3));
+```
+
+![](/Examples/example2.png)
+[example2.xlsx](Examples/example2.xlsx?raw=true)
+
+[More info in the test console application...](TestConsoleApp/Program.cs)
--- TestConsoleApp/Program.cs
+++ TestConsoleApp/Program.cs
@@ -10,7 +10,45 @@ namespace TestConsoleApp
     {
         static void Main(string[] args)
         {
-            var items = Enumerable.Range(1, 100).Select(x => new 
+            Example1();
+            Example2();
+            TestTypes();
+        }
+
+        static void Example1()
+        {
+            var items = Enumerable.Range(1, 10).Select(x => new
+            {
+                Prop1 = $"Text #{x}",
+                Prop2 = x * 1000,
+                Prop3 = DateTime.Now.AddDays(-x),
+            });
+
+            var excel = items.ToExcel();
+
+            File.WriteAllBytes(@"..\..\..\..\Examples\example1.xlsx", excel);
+        }
+
+        static void Example2()
+        {
+            var items = Enumerable.Range(1, 10).Select(x => new
+            {
+                Prop1 = $"Text #{x}",
+                Prop2 = x * 1000,
+                Prop3 = DateTime.Now.AddDays(-x),
+            });
+
+            var excel = items.ToExcel(x => x
+                .AddColumn("MyColumnName#1", x => x.Prop1)
+                .AddColumn("MyColumnName#2", x => $"test:{x.Prop2}")
+                .AddColumn("MyColumnName#3", x => x.Prop3));
+
+            File.WriteAllBytes(@"..\..\..\..\Examples\example2.xlsx", excel);
+        }
+
+        static void TestTypes()
+        {
+            var items = Enumerable.Range(1, 100).Select(x => new
             {
                 Bool = true,
                 Int = -1,
@@ -28,7 +66,7 @@ namespace TestConsoleApp
 
             //var data = items.ToExcel(x => x
             //    .AddColumn("MyColumn#1", x => x.Int)
-            //    .AddColumn("MyColumn#2", x => x.Bool)
+            //    .AddColumn("MyColumn#2", x => x.Bool,40)
             //    .AddColumn("MyColumn#3", x => x.String));
 
             File.WriteAllBytes(@".\test.xlsx", data);
